0afa21b08cfb05fc5beace77b1680cf4b7be5af4
Subham Kumar Mondal <76049195+subhamportblair@users.noreply.github.com>
2026-03-22 14:10:55 +0000

    commiting the changes with commenting  intent of failure

--- addition.py
+++ addition.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(2,3) == 5
     assert add(-1,1) == 0
     assert add(0,0) == 0
-    assert add(1,1) == 5  # This will FAIL (should be 2, not 5)
+    #assert add(1,1) == 5  # This will FAIL (should be 2, not 5)
 
     print("All tests passed!")
 
